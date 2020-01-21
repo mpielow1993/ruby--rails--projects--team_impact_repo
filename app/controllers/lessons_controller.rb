102f8a11792bc950f4b455c4d1811fbb41a2c2eb
mpielow1993 <michaelpielow@hotmail.com>
2020-01-21 19:25:11 +0000

    Implement ajax within registrations

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,24 +1,29 @@
 class LessonsController < ApplicationController
     before_action :logged_in_member
     before_action :correct_member, only: :private_timetable
-  
-  
+    #respond_to :html, :js
+
+
   def public_timetable
     @date = current_lesson_date
     #@subscription = current_subscription
     @lessons = Array.wrap(Lesson.where(date: @date))
     @registration = Registration.new
   end
-  
+
   def private_timetable
     @date = current_lesson_date
     @lessons = Array.wrap(current_member.lessons.where(date: @date))
   end
-  
+
   def search_timetable
     session[:lesson_date] = params[:search][:lesson_date]
     #session[:subscription_id] = params[:search][:subscription_id]
     redirect_to request.referrer
+  end
+
+  def reg_flash
+    flash.dup
   end
 
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,23 @@
 class RegistrationsController < ApplicationController
-before_action :logged_in_member
+  #respond_to :html, :js
+  before_action :logged_in_member
 
   def create
     @date = current_lesson_date
     @lesson = Lesson.find(params[:lesson_id])
     @registration = @lesson.registrations.build(registration_params)
     if @registration.save
-      flash[:success] = "You have successfully registered for the chosen lesson"
-      redirect_to member_private_timetable_path(current_member, @date)
+      flash.dup[:success] = " Registration Successful"
+      respond_to do |format|
+        format.html {}
+        format.js {}
+      end
     else
-      flash[:danger] = "You have already registered for this lesson with subscription '#{current_member.subscription_used_to_register_for(@lesson)}'"
-      redirect_to request.referrer
+      flash.dup[:danger] = "Already registered with subscription '#{current_member.subscription_used_to_register_for(@lesson).subscription_name}'"
+      respond_to do |format|
+        format.html {}
+        format.js {}
+      end
     end
   end
 
@@ -19,13 +26,20 @@ before_action :logged_in_member
     @lesson = Lesson.find(params[:lesson_id])
     @registration = @lesson.registrations.find(params[:id])
     @registration.destroy
-    flash[:success] = "You have successfully unregistered"
-    redirect_to public_timetable_path(@date)
+    flash.dup[:success] = "Deregistration Successful"
+    respond_to do |format|
+      format.html {}
+      format.js {}
+    end
   end
-  
+
   private
-    
+
     def registration_params
       params.require(:registration).permit(:subscription_id, :member_id)
+    end
+
+    def reg_flash
+      flash.dup
     end
 end
