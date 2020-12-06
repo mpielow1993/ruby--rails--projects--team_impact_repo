dac0a8cea0911426493040bd635567957d4046fc
mpielow1993 <michaelpielow@hotmail.com>
2020-12-06 16:35:20 +0000

    Working search form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   include ApplicationHelper
-
-  protected
-
-    # Filter method
-    # UNIT TEST
-    def search(relation, params)
-      return relation if params.empty?
-      relation.where(params)
-    end
-
-    # Define parameters required by the user to save record
-    def set_required_params(search_param_keys_array)
-      params.fetch(
-        controller_name.classify.to_s.underscore.to_sym, 
-        *controller_name.classify.constantize.fields
-        ).permit(search_param_keys_array)  
-    end
-
-    # Define parameters used in Filter method
-    def set_search_params(search_param_keys_array)
-      params.fetch(
-        controller_name.classify.to_s.underscore.to_sym, 
-      *controller_name.classify.constantize.fields
-      ).permit(search_param_keys_array)
-    end
   
     private
 
