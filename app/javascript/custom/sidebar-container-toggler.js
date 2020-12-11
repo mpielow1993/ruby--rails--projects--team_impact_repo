540f1703900e80a10afae14bfb7b6ac9412fd1c1
mpielow1993 <michaelpielow@hotmail.com>
2020-12-11 20:16:24 +0000

    Improve sidebar

--- app/javascript/custom/sidebar-container-toggler.js
+++ app/javascript/custom/sidebar-container-toggler.js
@@ -2,14 +2,14 @@ $(function() {
 	$("#sidebar-container-toggler").click(function(e) {
 		e.preventDefault();
 		$("#grid-container")
-			.toggleClass("show-sidebar", 2000);
+			.toggleClass("show-sidebar");
 		$('.glyphicon', this)
 			.toggleClass('glyphicon-chevron-right')
 	})
 	.click(function(e) {
 		e.preventDefault();
 		$("#grid-container")
-			.toggleClass("hide-sidebar", 2000);
+			.toggleClass("hide-sidebar");
 		$('.glyphicon', this)
 			.toggleClass('glyphicon-chevron-left')
 	});
