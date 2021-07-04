5cfd6b900120bcb6e8f418d431def8ea26f7bc96
mpielow1993 <michaelpielow@hotmail.com>
2021-07-04 21:47:39 +0100

    correct env varaibles for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => '<SENDGRID_API_KEY>', # This is the secret sendgrid API key which was issued during API key creation
+  :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'team-impact-app.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
