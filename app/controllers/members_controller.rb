db129462a7d4a13c8aae9156e266d4b37ac014aa
mpielow1993 <michaelpielow@hotmail.com>
2021-07-04 17:15:36 +0100

    edit member_avatar input

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -37,8 +37,8 @@ class MembersController < ApplicationController
   
   private
   
-  def member_params
-    params.require(:member).permit(:user_name, :first_name, :last_name, :email, :phone_no, :password, :password_confirmation, :member_avatar, :remove_member_avatar)
-  end
+    def member_params
+      params.require(:member).permit(:user_name, :first_name, :last_name, :email, :phone_no, :password, :password_confirmation, :avatar, :remove_avatar)
+    end
   
 end
