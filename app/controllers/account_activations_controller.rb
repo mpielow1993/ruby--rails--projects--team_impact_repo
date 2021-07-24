0ce67f3953ad72e4c95b5ee7c5e95d4c9f9a378b
mpielow1993 <michaelpielow@hotmail.com>
2021-07-24 18:18:46 +0100

    final change before pentest

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,14 +2,14 @@ class AccountActivationsController < ApplicationController
     
     #An 'edit' action to activate accounts
     def edit 
-        @member = Member.find_by(user_name: params[:user_name]) 
-        check_existence(@member, root_url, "Member not found")
-        if @member.authenticated?(:activation, params[:id]) 
+        member = Member.find_by(user_name: params[:user_name]) 
+        check_existence(member, root_url, "Member not found")
+        if member.authenticated?(:activation, params[:id]) 
             #Account activation via the member model object
-            @member.activate
-            log_in @member 
+            member.activate
+            log_in member 
             flash[:success] = "Account Activated!" 
-            redirect_to @member 
+            redirect_to member 
         else 
             flash[:danger] = "Invalid Activation Link" 
             redirect_to root_url 
