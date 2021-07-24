961659743355e5a1df135ad9aea578ee7e053233
mpielow1993 <michaelpielow@hotmail.com>
2021-07-24 14:16:57 +0100

    final change before pentest

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -12,8 +12,8 @@ class MembersController < ApplicationController
   
   def create
     @member = Member.new(member_params) 
+    @member.create_activation_digest
     if @member.save
-      @member.create_activation_digest
       @member.send_activation_email
       flash[:info] = "Please check your email to activate your Team Impact account."
       redirect_to root_url
