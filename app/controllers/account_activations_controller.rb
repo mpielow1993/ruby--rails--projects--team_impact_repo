0440abb2606b530c904df219a5aa38a625403faa
mpielow1993 <michaelpielow@hotmail.com>
2021-07-24 17:50:44 +0100

    changes

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,17 +4,14 @@ class AccountActivationsController < ApplicationController
     def edit 
         @member = Member.find_by(user_name: params[:user_name]) 
         check_existence(@member, root_url, "Member not found")
-        if @member.activated == false && @member.authenticated?(:activation, params[:id]) 
-            #member.update_attribute(:activated, true) 
-            #member.update_attribute(:activated_at, Time.zone.now) 
-            
+        if !@member.activated? && @member.authenticated?(:activation, params[:id]) 
             #Account activation via the member model object
             @member.activate
             log_in @member 
             flash[:success] = "Account Activated!" 
-            redirect_to @member 
+            redirect_to member 
         else 
-            flash[:danger] = "#{@member.activated} + #{@member.authenticated?(:activation, params[:id])}" 
+            flash[:danger] = "Invalid Activation Link" 
             redirect_to root_url 
         end 
     end
